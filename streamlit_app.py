dfb9eb9437a5eeb1f286f7c311e8fb97c70bbdad
tklim-learn <73205796+tklim-learn@users.noreply.github.com>
2024-09-17 14:52:38 +0800

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
-#session = get_active_session()
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
